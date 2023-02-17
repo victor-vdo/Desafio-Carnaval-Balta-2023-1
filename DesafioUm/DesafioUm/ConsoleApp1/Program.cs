10ef9a3dff5005ddb057f15c2b09fa945d209c5a
victor-vdo <37857818+victor-vdo@users.noreply.github.com>
2023-02-17 15:56:28 -0300

    Removendo caracteres especiais

---
+++ DesafioUm/DesafioUm/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Digite um texto:");
+var texto = Console.ReadLine();
+var novoTexto = texto;
+Dictionary<char, string> dic = new Dictionary<char, string>()
+{
+    { '.', String.Empty}, { ',', String.Empty}, { ':', String.Empty}, { ';', String.Empty},
+    { '>', String.Empty}, { '<', String.Empty}, { '{', String.Empty}, { '}', String.Empty},
+    { '(', String.Empty}, { ')', String.Empty}, { '[', String.Empty}, { ']', String.Empty},
+    { '?', String.Empty}, { '!', String.Empty}, { '/', String.Empty}, { '|', String.Empty}
+};
+
+foreach (var charac in texto)
+{
+    if (dic.ContainsKey(charac))
+        novoTexto = novoTexto.Replace(charac.ToString(), String.Empty);
+}
+
+var palavras = novoTexto.Split(" ");
+Console.WriteLine(texto.Length + " caracteres, " + palavras.Length + " palavras");
--- DesafioUm/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Digite um texto:");
-var texto = Console.ReadLine().Trim();
-var palavras = texto.Split(" ");
-
-Console.WriteLine(texto.Length +" caracteres, " + palavras.Length + " palavras");
